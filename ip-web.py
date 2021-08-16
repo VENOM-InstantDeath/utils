f99968e8c848e18f135ab723f7fb15d7dd83d374
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-08-16 01:33:24 -0300

    Minor update ip-web

--- ip-web.py
+++ ip-web.py
@@ -4,7 +4,7 @@ import sys
 def main():
 	try:
 		ip = socket.gethostbyname(sys.argv[1])
-		return f"\033[92m [+]URL: {sys.argv[1]}\n[+]IP: {ip}\033[00m"
+		return f"\033[92m[+]URL: {sys.argv[1]}\n[+]IP: {ip}\033[00m"
 	except IndexError:
 		return "\033[91m[-]Debes especificar una url\033[00m"
 	except socket.gaierror:
